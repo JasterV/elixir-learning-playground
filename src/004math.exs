b3afcff005056a58f788a8da7ed4f433d3f2d431
JasterV <jaster.victor@gmail.com>
2022-03-20 23:49:50 +0100

    first commit

--- src/004math.exs
+++ src/004math.exs
@@ -1,8 +1,8 @@
 defmodule Math do
   def zero?(0), do: true
-  def zero?(x), when is_integer(x), do: false
+  def zero?(x) when is_integer(x), do: false
   def zero?(_), do: false
 end
 
-IO.inspect(Math.div(1, 0))
-IO.inspect(Math.div(1, 2))
+IO.inspect(Math.zero?(1))
+IO.inspect(Math.zero?(2))
---
+++ src/pipes.exs
@@ -0,0 +1,21 @@
+defmodule PipeTest do
+  # Private functions
+  defp square(x) when is_number(x), do: x * x
+
+  defp sum(l, start \\ 0) when is_number(start) and is_list(l) do
+    start + Enum.sum(l)
+  end
+
+  # Public functions
+  def sst(list) do
+    # Get the square of the sum of the tail of a list
+    list
+    |> tl
+    # |> IO.inspect()
+    |> sum
+    # |> IO.inspect()
+    |> square
+  end
+end
+
+IO.puts(PipeTest.sst([1, 2, 3, 4]))
